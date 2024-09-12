d8f00057866da0e7e2e12dc609834246d3c10423
im_ayushjha <ayushjha2805@gmail.com>
2024-09-12 19:50:33 +0530

    added reset button logic

--- Public/function.js
+++ Public/function.js
@@ -113,3 +113,44 @@ function update() {
     check(event);
   });
 }
+
+const spans = document.getElementsByTagName("span");
+spans[0].classList.add("highlight");
+
+document.querySelector(".reset").addEventListener("click", reset);
+function reset() {
+  i = 0;
+  j = 0;
+  index = 0;
+  b = "";
+  ////////timer reset///////////////
+  total = 0;
+  count = 0;
+  time = currenttime;
+  clearInterval(y);
+  y = null;
+  document.querySelector(".timer").innerHTML = "&nbsp;";
+  document.querySelector(".text").disabled = false;
+  document.querySelector(".text").autofocus = true;
+  ///////timer reset///////////////
+
+  /////////shuffle//////////////////
+  words.sort(() => Math.random() - 0.5);
+  //////////////////////////////////////////
+
+  resettextarea();
+  const paraelement = document.querySelector(".one");
+  paraelement.innerHTML = "";
+  for (var k = 0; k < words.length; k++) {
+    const span = document.createElement("span");
+    span.innerText = words[k] + " ";
+    paraelement.appendChild(span);
+  }
+  const spans = document.getElementsByTagName("span");
+  spans[0].classList.add("highlight");
+  document.getElementById("typing-area").focus();
+}
+
+function resettextarea() {
+  document.querySelector(".text").value = "";
+}
